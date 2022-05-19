912fa90da7760321d1da18f1d4839ae7d76dd50d
AntonioWilliam <88805304+AntonioWilliam@users.noreply.github.com>
2022-05-19 20:13:41 -0300

    Update biblioteca.sql

--- biblioteca.sql
+++ biblioteca.sql
@@ -1,20 +1,15 @@
+CREATE DATABASE biblioteca;
 
 CREATE TABLE livro (
 id serial primary key,
 titulo varchar (100)
 );
 
-
-
 CREATE TABLE usuario (
 id serial primary key,
 nome varchar (100) not null,
 idade INTEGER
 );
-
-
-
-
 
 CREATE TABLE emprestimo (
 id serial primary key,
@@ -24,10 +19,6 @@ id_do_usuario INTEGER UNIQUE NOT NULL,
 FOREIGN key (id_do_usuario) REFERENCES usuarios(id),
 datae DATE
 );
-
-
-
-
 
 insert into livro (titulo) values ('1984');
 insert into livro (titulo) values ('Revolução dos bichos');
@@ -39,10 +30,6 @@ insert into livro (titulo) values ('O estrangeiro');
 insert into livro (titulo) values ('Linguagem de programação vol.2');
 insert into livro (titulo) values ('Rádio e TV');
 insert into livro (titulo) values ('Jornalista');
-
-
-
-
 
 insert into usuario (nome, idade) values ('William A.', 21);
 insert into usuario (nome, idade) values ('Bruna A.', 18);
@@ -56,30 +43,16 @@ insert into usuario (nome, idade) values ('Igor L.', 20);
 insert into usuario (nome, idade) values ('Kaue A.', 20);
 insert into usuario (nome, idade) values ('Breno A.', 21);
 
-
-
-
-
 INSERT into emprestimo (id_do_livro, id_do_usuario, datae) VALUES (1,2, '2022-10-22');
 INSERT into emprestimo (id_do_livro, id_do_usuario, datae) VALUES (5,6, '2022-10-22');
 INSERT into emprestimo (id_do_livro, id_do_usuario, datae) VALUES (6,5, '2022-10-22');
 INSERT into emprestimo (id_do_livro, id_do_usuario, datae) VALUES (9,3, '2022-10-22');
 
-
-
-
-
-
-
 SELECT l.titulo as TiTulo_do_Livro, u.nome as Nome, e.datadoemprestimos from emprestimos as e
 join usuario as u
 on u.nome== e.id_do_usuario;
 
-
-
 join livro as l
 on l.titulo== e.id_do_livro;
-
-
 
 SELECT emprestimo WHERE u.idade <18;
